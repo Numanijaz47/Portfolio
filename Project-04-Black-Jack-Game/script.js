ace99529ad4fef058ffa3d47aa06e7b5411da2da
Numan Ijaz47 <numan0ijaz@gmail.com>
2025-09-13 22:11:10 +0500

    123

--- Project-04-Black-Jack-Game/script.js
+++ Project-04-Black-Jack-Game/script.js
@@ -1,13 +1,20 @@
-let firstCard = 1;
-let secondCard = 11;
+let firstCard = 10;
+let secondCard = 12  ;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
-console.log(sum);
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card?");
+    console.log("Do you want to draw a new card? 😊");
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!");
+    console.log("Wohoo! You've got Blackjack! 😍");
+    hasBlackJack = true;
 } else if (sum > 21) {
-    console.log("You're out of the game!");
+    console.log("You're out of the game! 😢");
+    isAlive = false;
 }
+
+console.log(hasBlackJack);
+
+
