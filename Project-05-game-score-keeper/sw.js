6c81cdb185da9f163a9c2a36bd5ca3a54372beb3
Numan Ijaz47 <numan0ijaz@gmail.com>
2025-09-13 22:28:38 +0500

    123

--- Project-05-game-score-keeper/sw.js
+++ Project-05-game-score-keeper/sw.js
@@ -4,10 +4,15 @@ self.addEventListener('install', event => {
       return cache.addAll([
         '/',
         '/index.html',
+        '/manifest.json', // Manifest file
         '/css/style.css',
         '/js/script.js',
         '/icon-192.png',
-        '/icon-512.png'
+        '/icon-512.png',
+        // Caching Bootstrap & Icons from CDN
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
       ]);
     })
   );
